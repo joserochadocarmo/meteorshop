aed45a4bbb54587cf516cc890a5bfb5bf17e8010
Jose Rocha <tom.jrc@gmail.com>
2015-07-01 21:39:46 -0300

    initial...

---
+++ collections.js
@@ -0,0 +1,6 @@
+Categories = new Mongo.Collection('categories');
+SubCategories = new Mongo.Collection('subcategories');
+Products = new Mongo.Collection('products');
+Customer = new Mongo.Collection('customers');
+Cart = new Mongo.Collection('cart');
+CartItems  = new Mongo.Collection('cartitems');
